a5576bf10cdb915b37b594045cd4ca0f7f0ab968
Leonardo <lfm.fiap@gmail.com>
2024-11-30 22:41:13 -0300

    Editando.

--- src/js/menu.js
+++ src/js/menu.js
@@ -12,6 +12,20 @@ document.getElementById('btnFechar').addEventListener('click', function() {
     detalhes.classList.add('hidden'); // Recolher a seção "Detalhes" com animação
 });
 
+// Função para animar a transição de exibição do conteúdo "Detalhes"
+document.getElementById('btnSaibaMais2').addEventListener('click', function() {
+    const detalhes = document.getElementById('detalhes2');
+    detalhes.classList.add('visible2'); // Tornar a seção "Detalhes" visível com animação
+    detalhes.classList.remove('hidden2');
+});
+
+// Função para fechar o conteúdo "Detalhes"
+document.getElementById('btnFechar2').addEventListener('click', function() {
+    const detalhes = document.getElementById('detalhes2');
+    detalhes.classList.remove('visible2');
+    detalhes.classList.add('hidden2'); // Recolher a seção "Detalhes" com animação
+});
+
 // Efeito de mudança de cor no cabeçalho quando o mouse passa sobre o menu
 const navLinks = document.querySelectorAll('nav ul li a');
 navLinks.forEach(link => {
